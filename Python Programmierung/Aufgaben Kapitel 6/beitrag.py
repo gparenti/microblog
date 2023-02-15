63218da6a122e1328b6c20471b3939a22113d50f
gparenti <gioele.parenti@gmail.com>
2023-02-15 20:21:23 +0100

    neues skript

---
+++ Python Programmierung/Aufgaben Kapitel 6/beitrag.py
@@ -0,0 +1,14 @@
+zahl = int(input("Zahl:"))
+zins1 = 10
+zins2 = 5
+if zahl < 20000:
+    print("Abzug: 0")
+elif zahl >= 20000 and zahl <= 120000:
+    if zahl > 20000 and zahl <= 80000:
+        abzug = (zins1/100) * zahl
+        print("Abzug:",abzug)
+    elif zahl > 80000:
+        abzug = (zins2/100) * zahl
+        print("Abzug:",abzug)
+else:
+    print("Fehler")
