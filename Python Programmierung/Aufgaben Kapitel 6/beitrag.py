449aa28d21f0999d63bc220b0d929c3b671152a1
gparenti <gioele.parenti@gmail.com>
2023-02-19 12:38:44 +0100

    Programm korrigiert

--- Python Programmierung/Aufgaben Kapitel 6/beitrag.py
+++ Python Programmierung/Aufgaben Kapitel 6/beitrag.py
@@ -1,15 +1,29 @@
-zahl = int(input("Zahl:"))
-zins1 = 10
-zins2 = 5
+lohninput = float(input("lohninput:"))
 
-if zahl < 20000:
-    print("Abzug: 0")
-elif zahl >= 20000 and zahl <= 120000:
-    if zahl > 20000 and zahl <= 80000:
-        abzug = (zins1/100) * zahl
-        print("Abzug:",abzug)
-    elif zahl > 80000:
-        abzug = (zins2/100) * zahl
-        print("Abzug:",abzug)
-else:
-    print("Kein Abzug")
+zins10 = 10
+zins5 = 5
+
+grenze1 = 20000
+grenze2 = 80000
+grenze3 = 120000
+
+if lohninput < grenze1:
+    print("Abzug: 0 - Lohn unter 20'000")
+
+if lohninput >= grenze1 and lohninput <= grenze2:
+        abzug1 = (lohninput/100) * zins10
+        print("Abzug:",abzug1)
+
+elif lohninput > grenze3:
+    lohninput = grenze3
+
+if lohninput > grenze2:
+    lohninputneu = lohninput - 80000
+    abzug1 = (grenze2/100) * zins10
+    print("Abzug1:",abzug1)
+
+    abzug2 = (zins5/100) * lohninputneu
+    print("Abzug2:",abzug2)
+
+    output = abzug1 + abzug2
+    print("Abzug:",output)
