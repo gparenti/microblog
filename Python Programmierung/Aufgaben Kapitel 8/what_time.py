fd1d782514756a66ed2c42559a24dc168ce4e548
gparenti <gioele.parenti@gmail.com>
2023-03-13 20:52:22 +0100

    erweiterung

--- Python Programmierung/Aufgaben Kapitel 8/what_time.py
+++ Python Programmierung/Aufgaben Kapitel 8/what_time.py
@@ -4,10 +4,11 @@ today = datetime.datetime.now()
 hour = today.hour
 minute = today.minute
 
-
 if hour >= 12:
     hour -= 12
 if minute < 30:
     print(minute,"nach",hour)
 elif minute > 30:
     print(60-minute, "vor", hour + 1)
+elif minute == 30:
+    print("halb",hour)
