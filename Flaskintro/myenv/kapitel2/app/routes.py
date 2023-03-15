6df5b62421bb8da4591c990cfc9ddb64fb0f432e
gparenti <gioele.parenti@gmail.com>
2023-03-15 19:28:30 +0100

    n

--- Flaskintro/myenv/kapitel2/app/routes.py
+++ Flaskintro/myenv/kapitel2/app/routes.py
@@ -4,7 +4,12 @@ from app import app
 def index():
  return "<h1>Hello, Flask!</h1>"
 
-
 @app.route('/user/<name>')
 def user(name):
  return '<h1>Hello, {}!</h1>'.format(name)
+
+from flask import request
+@app.route ('/useragent')
+def uagent():
+ user_agent = request.headers.get('User-Agent')
+ return '<p> Ihr Browser: {}</p>'.format(user_agent)
