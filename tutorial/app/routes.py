75b67af7d027a58d60d0d9ad6ed2b1f811aadc25
gparenti <gioele.parenti@gmail.com>
2023-05-03 19:36:53 +0200

    app route explore von tutorial übernommen

--- tutorial/app/routes.py
+++ tutorial/app/routes.py
@@ -32,15 +32,15 @@ def index():
 @app.route('/explore')
 @login_required
 def explore():
-   page = request.args.get('page', 1, type=int)
-   posts = Post.query.order_by(Post.timestamp.desc()).paginate(
+    page = request.args.get('page', 1, type=int)
+    posts = Post.query.order_by(Post.timestamp.desc()).paginate(
         page=page, per_page=app.config['POSTS_PER_PAGE'], error_out=False)
-   next_url = url_for('explore', page=posts.next_num) \
-      if posts.has_next else None
-   prev_url = url_for('explore', page=posts.prev_num) \
-      if posts.has_prev else None
-   return render_template("index.html", title='Explore', posts=posts.items,
-                          next_url=next_url, prev_url=prev_url)
+    next_url = url_for('explore', page=posts.next_num) \
+        if posts.has_next else None
+    prev_url = url_for('explore', page=posts.prev_num) \
+        if posts.has_prev else None
+    return render_template('index.html', title='Explore', posts=posts.items,
+                           next_url=next_url, prev_url=prev_url)
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
